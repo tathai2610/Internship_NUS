0852f8bbf28e697557b804e1f852dc4eb4b64303
thai <tathai2610@gmail.com>
2020-09-01 14:42:54 +0700

    update mailer

--- final_app/config/environments/development.rb
+++ final_app/config/environments/development.rb
@@ -64,9 +64,9 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'example.com',
-  user_name:            'thaibach33@gmail.com',
-  password:             'thaiai113',
+  domain:               'gmail.com',
+  user_name:            'groupsoftwaremulti@gmail.com',
+  password:             'softwaremulti',
   authentication:       'plain',
   enable_starttls_auto: true }
 end
