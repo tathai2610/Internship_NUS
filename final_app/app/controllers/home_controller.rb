c16578e68784411160d42ae50e80b5f8676f582e
thai <tathai2610@gmail.com>
2020-09-03 00:40:04 +0700

    Update UI

--- final_app/app/controllers/home_controller.rb
+++ final_app/app/controllers/home_controller.rb
@@ -4,4 +4,7 @@ class HomeController < ApplicationController
     @albums = Album.all.order(created_at: :desc).limit(4)
   end
 
+  def feed_photo
+    @photos = Photo.where(shared:true).order(created_at: :desc)
+  end
 end
